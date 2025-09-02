54539b65255aeafdf0b2726f8e0a7a3dfb99be62
sleeva <bharg@Bhargavi>
2025-09-02 11:02:57 +0530

    code changed

--- terraform_files/taxi-infra.tf
+++ terraform_files/taxi-infra.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 data "aws_vpc" "default" {
   default = true
 }
@@ -15,82 +14,85 @@ data "aws_subnets" "default" {
 }
 
 resource "aws_instance" "ansible" {
-  ami                    = "ami-0bbdd8c17ed981ef9"
-  instance_type          = "t2.micro"
-  key_name               = "taxi"
-  vpc_security_group_ids = [aws_security_group.demo-sg.id]
-  //subnet_id               = "subnet-077471d3c705ea769"
-  tags = {
-    Name = "ansible"
-  }
+    ami                     = "ami-0bbdd8c17ed981ef9"
+    instance_type           = "t2.micro"
+    key_name                = "taxi"
+    vpc_security_group_ids  = [aws_security_group.demo-sg.id]
+    //subnet_id               = "subnet-077471d3c705ea769"
+    tags                    = {
+        Name      = "ansible"
+    }
 }
+    
+
 
 resource "aws_instance" "jenkins_master" {
-  ami                    = "ami-0bbdd8c17ed981ef9"
-  instance_type          = "t2.medium"
-  key_name               = "taxi"
-  vpc_security_group_ids = [aws_security_group.demo-sg.id]
-
-  tags = {
+  ami                        = "ami-0bbdd8c17ed981ef9"
+  instance_type              = "t2.medium"
+  key_name                   = "taxi"
+  vpc_security_group_ids     = [aws_security_group.demo-sg.id]
+  
+  tags                       = {
     Name = "jenkins-master"
   }
+  
+
 }
 
 resource "aws_instance" "jenkins_slave" {
-  ami                    = "ami-0bbdd8c17ed981ef9"
-  instance_type          = "t2.medium"
-  key_name               = "taxi"
-  vpc_security_group_ids = [aws_security_group.demo-sg.id]
-
-  tags = {
+  ami                        = "ami-0bbdd8c17ed981ef9"
+  instance_type              = "t2.medium"
+  key_name                   = "taxi"
+  vpc_security_group_ids     = [aws_security_group.demo-sg.id]
+  
+  tags                       = {
     Name = "jenkins-slave"
   }
+  
 }
+
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
 
+  
   ingress {
-    description = "SHH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    description      = "SHH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    description = "Jenkins port"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Container  port"
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "https  port"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "http  port"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+    ingress {
+    description      = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ingress {
+    description      = "Container  port"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ingress {
+    description      = "https  port"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+    ingress {
+    description      = "http  port"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
   egress {
     from_port        = 0
     to_port          = 0
@@ -98,11 +100,13 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
   tags = {
     Name = "ssh-port"
+
   }
 }
- module "sgs" {
+module "sgs" {
   source = "../sg_eks"
   vpc_id = data.aws_vpc.default.id
 }
@@ -119,3 +123,4 @@ module "eks" {
 output "eks_cluster_endpoint" {
   value = module.eks.endpoint
 }
+
